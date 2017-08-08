6d00d14329a4ca330e66322cb5110880e9f571be
Rails Udemy Course <unmesh_anant@yahoo.com>
2017-08-08 20:42:29 +0000

    Add, edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -12,10 +17,29 @@ class ArticlesController < ApplicationController
     end
   end
   
+  
   def show
     @article = Article.find(params[:id])
   end
-
+  
+  
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else 
+      render 'edit'
+    end
+  end
+  
+  
   private
     def article_params
       params.require(:article).permit(:title, :description) 
